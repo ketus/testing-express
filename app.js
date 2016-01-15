c258ba82620632e35ee13fda90e298571b9fcdf5
Ketus <maciej.oleksa@gmail.com>
2016-01-15 18:05:26 +0100

    testing POST

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var jade = require('jade');
-var app = express();
+var bodyParser = require('body-parser');
 
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 var port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/front/views');
@@ -9,6 +11,12 @@ app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render('index', { title: req.params.test });
+});
+
+app.post('/post-test', urlencodedParser, function (req, res) {
+  res.send('Yo!');
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
 });
 
 app.get('/:test', function (req, res, next) {
